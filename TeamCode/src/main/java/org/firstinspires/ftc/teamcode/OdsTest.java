02ea684cf447b294ca4620ede706aab1414f8413
Erin Zhao <erin_zhao@horacemann.org>
2018-11-30 16:51:05 -0500

    TEST CODES FOR RANG EAND ODS

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdsTest.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+@TeleOp(name = "ODS Test", group = "LinearOpMode")
+public class OdsTest extends LinearOpMode {
+
+    OpticalDistanceSensor odsSensor;  // Hardware Device Object
+
+    @Override
+    public void runOpMode() {
+
+        // get a reference to our Light Sensor object.
+        odsSensor = hardwareMap.get(OpticalDistanceSensor.class, "ODS");
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // while the op mode is active, loop and read the light levels.
+        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+        while (opModeIsActive()) {
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Raw",    odsSensor.getRawLightDetected());
+            telemetry.addData("Normal", odsSensor.getLightDetected());
+
+            telemetry.update();
+        }
+    }
+}
